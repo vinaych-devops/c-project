fba42e9bfc5de6f9d1cc1942cad862f10231fed0
vinaych-devops <138816115+vinaych-devops@users.noreply.github.com>
2023-07-11 22:16:34 +0530

    Create add2.c

---
+++ add2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void sumof2() {    
+
+    int number1, number2, sum;
+
+    printf("\n\nEnter two integers: ");
+    scanf("%d %d", &number1, &number2);
+
+    // calculate the sum
+    sum = number1 + number2;      
+
+    printf("%d + %d = %d", number1, number2, sum);
+  //  return 0;
+}
